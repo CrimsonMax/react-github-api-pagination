9768e256350e122a866e759c7214854777c5f3f8
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2021-05-23 03:49:30 +0300

    Initial stage

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.less'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>React activated!</h1>
+    </div>
+  )
+}
+
+export default App
